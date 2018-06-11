ac0da9244f8a37243ec4a31aad5ce5ecbcbd91dd
Wampa842 <wampa842@gmail.com>
2018-06-11 21:34:54 +0200

    added license info

--- UnflipYourRide/UnflipYourRide.cs
+++ UnflipYourRide/UnflipYourRide.cs
@@ -1,4 +1,20 @@
-﻿using MSCLoader;
+﻿/*
+Copyright (C) <year>  <name of author>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using MSCLoader;
 using UnityEngine;
 
 namespace FlipYourRide
@@ -31,7 +47,7 @@ namespace FlipYourRide
 					return;
 				}
 
-				// If the argument is missing, not a number or out of range, display the help text.
+				// If the argument is missing, not a number, or out of range, display the help text.
 				int num;
 				if (args.Length < 1 || !int.TryParse(args[0], out num) || num < 0 || num >= vehicles.Length)
 				{
