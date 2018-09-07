47c7d098ab4cb0089bd7fa397132bb02398c479f
Wampa842 <wampa842@gmail.com>
2018-09-07 23:23:03 +0200

    added GameObjectTree and TwentyFourClock

---
+++ GameObjectTree/GameObjectTree.cs
@@ -0,0 +1,275 @@
+﻿using MSCLoader;
+using UnityEngine;
+using System;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using HutongGames.PlayMaker;
+
+namespace GameObjectTree
+{
+	public static class XmlifyExtensions
+	{
+		// Vector3
+		public static XmlElement Xmlify(this Vector3 v, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Vector3");
+
+			e.AppendChild(doc.CreateElement("X")).AppendChild(doc.CreateTextNode(v.x.ToString("0.00")));
+			e.AppendChild(doc.CreateElement("Y")).AppendChild(doc.CreateTextNode(v.y.ToString("0.00")));
+			e.AppendChild(doc.CreateElement("Z")).AppendChild(doc.CreateTextNode(v.z.ToString("0.00")));
+
+			return e;
+		}
+
+		// Quaternion
+		public static XmlElement Xmlify(this Quaternion q, XmlDocument doc)
+		{
+			return q.eulerAngles.Xmlify(doc);
+		}
+
+		// Component
+		public static XmlElement Xmlify(this Component c, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Component");
+			e.SetAttribute("type", c.GetType().ToString());
+			e.AppendChild(doc.CreateElement("Name")).AppendChild(doc.CreateTextNode(c.name));
+			return e;
+		}
+
+		// Transform
+		public static XmlElement Xmlify(this Transform t, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Transform");
+
+			e.AppendChild(doc.CreateElement("Position")).AppendChild(t.position.Xmlify(doc));
+			e.AppendChild(doc.CreateElement("Rotation")).AppendChild(t.position.Xmlify(doc));
+			e.AppendChild(doc.CreateElement("LocalPosition")).AppendChild(t.position.Xmlify(doc));
+			e.AppendChild(doc.CreateElement("LocalRotation")).AppendChild(t.position.Xmlify(doc));
+			e.AppendChild(doc.CreateElement("LocalScale")).AppendChild(t.position.Xmlify(doc));
+
+			return e;
+		}
+
+
+		// Collider
+		public static XmlElement Xmlify(this Collider coll, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Collider");
+			e.SetAttribute("type", coll.GetType().ToString());
+
+			e.AppendChild(doc.CreateElement("IsTrigger")).AppendChild(doc.CreateTextNode(coll.isTrigger.ToString()));
+			if (coll is MeshCollider)
+				e.AppendChild(doc.CreateElement("IsTrigger")).AppendChild(doc.CreateTextNode(((MeshCollider)coll).convex.ToString()));
+
+			return e;
+		}
+
+		// Rigidbody
+		public static XmlElement Xmlify(this Rigidbody rb, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Rigidbody");
+
+			e.AppendChild(doc.CreateElement("Kinematic")).AppendChild(doc.CreateTextNode(rb.isKinematic.ToString()));
+			e.AppendChild(doc.CreateElement("Mass")).AppendChild(doc.CreateTextNode(rb.mass.ToString()));
+
+			return e;
+		}
+
+		// AudioSource
+		public static XmlElement Xmlify(this AudioSource audio, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("AudioSource");
+
+			e.AppendChild(doc.CreateElement("ClipName")).AppendChild(doc.CreateTextNode(audio.clip.name));
+			e.AppendChild(doc.CreateElement("SpatialBlend")).AppendChild(doc.CreateTextNode(audio.spatialBlend.ToString()));
+			e.AppendChild(doc.CreateElement("Volume")).AppendChild(doc.CreateTextNode(audio.volume.ToString()));
+			e.AppendChild(doc.CreateElement("Loop")).AppendChild(doc.CreateTextNode(audio.loop.ToString()));
+
+			return e;
+		}
+	}
+
+	public class GameObjectTree : Mod
+	{
+		public override string ID => "GameObjectTree";
+		public override string Name => "GameObjectTree";
+		public override string Author => "Wampa842";
+		public override string Version => "1.0.0";
+		public override bool UseAssetsFolder => false;
+		public string FilePath => Path.Combine(ModLoader.GetModConfigFolder(this), "hierarchy.xml");
+		public string BlacklistPath => Path.Combine(ModLoader.GetModConfigFolder(this), "blacklist.txt");
+
+		public Settings WriteButton, EditBlacklistButton;
+		//public Settings WriteComponents, WriteTransforms, WriteFsm, WriteCollider, WriteRigidbody, WriteAudioSource, WriteOther;
+		//public Settings MaxDepth;
+		public List<string> Blacklist;
+
+
+		// FSM
+		public static XmlElement ReadFsm(PlayMakerFSM fsm, XmlDocument doc)
+		{
+			XmlElement e = doc.CreateElement("Fsm");
+			XmlElement n;
+
+			e.AppendChild(doc.CreateElement("Name")).AppendChild(doc.CreateTextNode(fsm.FsmName));
+
+			if (fsm.FsmVariables.BoolVariables.Length > 0)
+			{
+				n = doc.CreateElement("FsmBool");
+				foreach (FsmBool v in fsm.FsmVariables.BoolVariables)
+				{
+					n.AppendChild(doc.CreateElement(v.Name)).AppendChild(doc.CreateTextNode(v.Value.ToString()));
+				}
+				e.AppendChild(n);
+			}
+			if (fsm.FsmVariables.IntVariables.Length > 0)
+			{
+				n = doc.CreateElement("FsmInt");
+				foreach (var v in fsm.FsmVariables.IntVariables)
+				{
+					n.AppendChild(doc.CreateElement(v.Name)).AppendChild(doc.CreateTextNode(v.Value.ToString()));
+				}
+				e.AppendChild(n);
+			}
+			if (fsm.FsmVariables.FloatVariables.Length > 0)
+			{
+				n = doc.CreateElement("FsmFloat");
+				foreach (var v in fsm.FsmVariables.FloatVariables)
+				{
+					n.AppendChild(doc.CreateElement(v.Name)).AppendChild(doc.CreateTextNode(v.Value.ToString()));
+				}
+				e.AppendChild(n);
+			}
+			if (fsm.FsmVariables.StringVariables.Length > 0)
+			{
+				n = doc.CreateElement("FsmString");
+				foreach (var v in fsm.FsmVariables.BoolVariables)
+				{
+					n.AppendChild(doc.CreateElement(v.Name)).AppendChild(doc.CreateTextNode(v.Value.ToString()));
+				}
+				e.AppendChild(n);
+			}
+
+			return e;
+		}
+
+		public XmlElement ReadComponent(XmlDocument doc, Component c)
+		{
+			XmlElement e = doc.CreateElement("Component");
+			e.SetAttribute("type", c.GetType().ToString());
+			if (!string.IsNullOrEmpty(c.name))
+			{
+				XmlElement name = doc.CreateElement("Name");
+				name.AppendChild(doc.CreateTextNode(c.name));
+				e.AppendChild(name);
+			}
+
+			if (c is PlayMakerFSM)
+			{
+				e.AppendChild(ReadFsm((PlayMakerFSM)c, doc));
+			}
+			//else if (c is Collider)
+			//{
+			//	e.AppendChild(((Collider)c).Xmlify(doc));
+			//}
+			//else if (c is Rigidbody)
+			//{
+			//	e.AppendChild(((Rigidbody)c).Xmlify(doc));
+			//}
+			//else if (c is AudioSource)
+			//{
+			//	e.AppendChild(((AudioSource)c).Xmlify(doc));
+			//}
+			//else
+			//{
+			//	e.AppendChild(c.Xmlify(doc));
+			//}
+
+			return e;
+		}
+
+		public XmlElement ReadGameObject(XmlDocument doc, GameObject o, int depth)
+		{
+			XmlElement e = doc.CreateElement("GameObject");
+
+			// Object properties
+			XmlElement name = doc.CreateElement("Name");
+			name.AppendChild(doc.CreateTextNode(o.name));
+			e.AppendChild(name);
+
+			// Components
+			Component[] components = o.GetComponents<Component>().Where(c => !(c is Transform)).ToArray();
+			if (components != null && components.Length > 0)
+			{
+				XmlElement ce = doc.CreateElement("Components");
+				int count = 0;
+				//foreach (Component c in components)
+				//{
+				//	//if (c is Transform && !(bool)WriteTransforms.GetValue())
+				//	//	continue;
+				//	//if (c is PlayMakerFSM && !(bool)WriteFsm.GetValue())
+				//	//	continue;
+				//	if (c is PlayMakerFSM)
+				//	{
+				//		ce.AppendChild(ReadComponent(doc, c));
+				//		++count;
+				//	}
+				//}
+				ce.SetAttribute("count", count.ToString());
+				e.AppendChild(ce);
+			}
+
+			// Child objects
+			if (o.transform.childCount > 0)
+			{
+				XmlElement ce = doc.CreateElement("Children");
+				ce.SetAttribute("count", o.transform.childCount.ToString());
+				for (int i = 0; i < o.transform.childCount; ++i)
+				{
+					GameObject child = o.transform.GetChild(i).gameObject;
+					ce.AppendChild(ReadGameObject(doc, child, depth + 1));
+				}
+				e.AppendChild(ce);
+			}
+
+			return e;
+		}
+
+		public void WriteAllGameObjects()
+		{
+			ModConsole.Print("[GOT] start");
+			XmlDocument doc = new XmlDocument();
+			XmlElement root = doc.CreateElement("MSCGameObjects");
+			foreach (GameObject o in GameObject.FindObjectsOfType<GameObject>().Where(e => e.transform.parent == null))
+			{
+				root.AppendChild(ReadGameObject(doc, o, 0));
+			}
+			doc.AppendChild(root);
+			doc.Save(FilePath);
+			ModConsole.Print("[GOT] end");
+		}
+
+		public GameObjectTree()
+		{
+			WriteButton = new Settings("write", "Write to file", () => { WriteAllGameObjects(); });
+			EditBlacklistButton = new Settings("blacklist", "Edit blacklist", () => { System.Diagnostics.Process.Start(BlacklistPath); });
+
+			//WriteComponents = new Settings("components", "Write components", true);
+			//WriteTransforms = new Settings("transforms", "Write transforms", true);
+			//WriteFsm = new Settings("fsm", "Write PlayMaker components", true);
+			//WriteCollider = new Settings("colliders", "Write colliders", true);
+			//WriteRigidbody = new Settings("rigidbodies", "Write rigid bodies", true);
+			//WriteAudioSource = new Settings("audiosources", "Write audio sources", true);
+			//WriteOther = new Settings("others", "Write other components", true);
+			//MaxDepth = new Settings("depth", "Hierarchy depth (0 for infinite)", (int)0);
+		}
+
+		public override void ModSettings()
+		{
+			Settings.AddButton(this, WriteButton);
+		}
+	}
+}
---
+++ TwentyFourClock/TwentyFourClock.cs
@@ -0,0 +1,80 @@
+﻿/*
+Copyright (C) 2018 Wampa842
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.IO;
+using MSCLoader;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using Clock24Hour;
+
+namespace Clock24Hour
+{
+	public class Clock24
+	{
+		private GameObject _sun;
+		private FsmFloat _rot;
+		private GameObject _needleH, _needleM;
+		public Clock24()
+		{
+			_sun = GameObject.Find("SUN/Pivot");
+			_rot = _sun.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmFloat("Rotation");
+			_needleH = GameObject.Find("SuomiClock/Clock/hour/NeedleHour");
+			_needleM = GameObject.Find("SuomiClock/Clock/minute/NeedleMinute");
+		}
+
+		public float Hour12 => ((360.0f - _needleH.transform.localRotation.eulerAngles.y) / 30.0f + 2.0f) % 12;
+
+		public float Hour24 => (_rot.Value > 330.0f || _rot.Value <= 150.0f) ? Hour12 + 12.0f : Hour12;
+
+		public float Minute => (360.0f - _needleM.transform.localRotation.eulerAngles.y) / 6.0f;
+
+		public float Second => (Minute * 60) % 60;
+
+		public override string ToString() => $"{Mathf.Floor(Hour24).ToString("00")}:{Mathf.Floor(Minute).ToString("00")}";
+	}
+}
+
+namespace TwentyFourClock
+{
+	public class TwentyFourClock : Mod
+	{
+		public override string ID => "TwentyFourClock";
+		public override string Name => "24-hour clock";
+		public override string Author => "Wampa842";
+		public override string Version => "1.0.0";
+		public override bool UseAssetsFolder => false;
+
+		private Clock24 _clock;
+		private Rect _rect;
+		private GUIStyle _style;
+
+		public override void OnLoad()
+		{
+			_clock = new Clock24();
+			_rect = new Rect(Screen.width / 2 - 80.0f, 40.0f, 160.0f, 30.0f);
+			_style = new GUIStyle();
+			_style.fontSize = 20;	
+			_style.normal.textColor = Color.white;
+			_style.alignment = TextAnchor.MiddleCenter;
+		}
+
+		public override void OnGUI()
+		{
+			GUI.Label(_rect, _clock.ToString(), _style);
+		}
+	}
+}
